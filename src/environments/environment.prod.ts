81bd7122fd0d31904f495410e3cdd2c3236fb997
cristhianhuamanyauris <cristhianhuamanyauris@gmail.com>
2026-05-25 09:39:26 -0500

    nuevo supabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  //supabaseUrl: 'https://ytuuexyatxxylfmyueea.supabase.co',
   supabaseUrl: 'https://evqjgxrduultornlirlz.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2cWpneHJkdXVsdG9ybmxpcmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNTgzNDIsImV4cCI6MjA3OTczNDM0Mn0.cTQeYveHPyueGoQ4joLxikZmvOhvBIUILTL8IDIjn9w',
+  //supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2cWpneHJkdXVsdG9ybmxpcmx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxNTgzNDIsImV4cCI6MjA3OTczNDM0Mn0.cTQeYveHPyueGoQ4joLxikZmvOhvBIUILTL8IDIjn9w',
+  supabaseAnonKey: 'sb_publishable_joQlSoze_JAjo78NFaawgQ_HDgoZl_h',
 };
